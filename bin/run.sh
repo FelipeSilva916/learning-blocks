c3439aadf54d8ec7fde907fc4c8d93880e8e3f1d
Felipe Silva <87789520+FelipeSilva916@users.noreply.github.com>
2022-11-16 20:19:07 -0800

    Update run.sh

--- bin/run.sh
+++ bin/run.sh
@@ -5,7 +5,7 @@ IFS=$'\n\t'
 DIR="$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)/.."
 DIR="$(readlink -f "$DIR")"
 pip install -r frontend/requirements.txt
-source "${DIR}/venv/bin/activate"
+# source "${DIR}/venv/bin/activate"
 python frontend/manage.py runserver "0.0.0.0:8000"
 # python manage.py test
 #python manage.py migrate
